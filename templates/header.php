c15ddc25bd59270fb9a12313eb0fcb896aa3c63e
jimshare <jim@shareprogress.org>
2015-10-20 22:00:22 -0700

    Added share buttons

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,23 @@
     <!-- Navigation -->
     <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
         <div class="container">
+            <div class="share-buttons navbar-callapse navbar-left">
+            	<ul class="nav navbar-nav">
+            		<li>
+            			<div class='sp_fb_small sp_share_button' page_url="<?php echo get_permalink(); ?>"></div>
+            		</li>
+            		<li>
+            			<div class='sp_tw_small sp_share_button' page_url="<?php echo get_permalink(); ?>"></div>
+            		</li>
+            	</ul>
+            </div>
+
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
                     <i class="fa fa-bars"></i>
                 </button>
             </div>
-
+            
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
                 <ul class="nav navbar-nav">
