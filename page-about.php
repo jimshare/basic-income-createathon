cd945a8b80d8d268e24583002bd351fd9141b52e
jimshare <jim@shareprogress.org>
2015-11-04 22:07:10 -0800

    Removed extra file

--- page-about.php
+++
@@ -1,9 +0,0 @@
-    <!-- About Section -->
-    <section id="about" class="container content-section text-center">
-        <div class="row">
-            <h1><?php the_title(); ?></h1>
-            <div class="col-lg-8 col-lg-offset-2">
-                <?php the_content(); ?>
-            </div>
-        </div>
-    </section>
