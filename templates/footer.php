a08c80c5962eb3a866015bba071b5a6b450a2e8f
jimshare <jim@shareprogress.org>
2015-11-05 20:59:00 -0800

    Added About page link to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
     <!-- Footer -->
     <footer>
         <div class="container text-center">
-            <p>Copyright &copy; Universal Income Project 2015</p>
+            <p>Copyright &copy; <a href="/about">Universal Income Project</a> 2015</p>
         </div>
     </footer>
