36a3a5ee4485eccfdc46cda5c7c381fcea5ac004
jimshare <jim@shareprogress.org>
2015-12-09 19:39:59 -0800

    Updated list styles and fixed header handling

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,7 @@
 <?php
 if ( is_front_page() ) :
     get_template_part('templates/header', 'home');
-elseif ( is_page( 'about' ) ) :
-    get_template_part('templates/header', 'about');
-elseif ( is_page( 'organize' ) ) :
+elseif ( is_page_template( 'template-content.php' ) ) :
     get_template_part('templates/header', 'about');
 else:
     get_template_part('templates/header', 'createathon');
