b1d900565b832082face1ec0c6b7a0cfab633542
Justine Lam <justine.lam@gmail.com>
2015-12-17 01:32:40 -0300

    Switch to using a content block for the sponsors link (sponsor-content custom field)
    -Featured Image to populate the background hero image at the very top of the page
    -Added content-section to sponsors and createathon sections

--- templates/content-createathon.php
+++ templates/content-createathon.php
@@ -1,5 +1,8 @@
+
+<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+
     <!-- Intro Header -->
-    <header id="page-top" class="intro">
+    <header id="page-top" class="intro" style="background: url('<?php echo $image[0]; ?>') no-repeat bottom center scroll; background-size: cover;">
         <div class="intro-body">
             <div class="container">
                 <div class="row">
@@ -30,7 +33,7 @@
     </section>
 
     <!-- Create-A-Thon Section -->
-    <section id="createathon" class="text-center">
+    <section id="createathon" class="content-section text-center">
         <div class="createathon-section">
             <div class="container">
                 <div class="col-lg-8 col-lg-offset-2">
@@ -47,7 +50,7 @@
                 <h2>Register</h2>
                 <?php echo types_render_field("registration-information", array("output"=>"html")); ?>
                 <p>
-                	<iframe src="<?php echo types_render_field("eventbrite-link", array("output"=>"raw")); ?>" frameborder="0" height="370" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+                	<iframe src="<?php echo types_render_field("eventbrite-link", array("output"=>"raw")); ?>" frameborder="0" height="auto" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
                 </p>
                 <p class="note">Having trouble with the Eventbrite interface? <a href="<?php echo types_render_field("eventbrite-backup-link", array("output"=>"raw")); ?>" target="_blank">Click here to go to the event page directly.</a></p>
             </div>
@@ -55,14 +58,14 @@
     </section>
 
     <!-- Sponsors Section -->
-    <section id="sponsors" class="text-center">
+    <section id="sponsors" class="content-section text-center">
         <div class="sponsors-section">
             <div class="container">
 				<div class="col-lg-8 col-lg-offset-2">
 					<h2>Sponsors</h2>
 					<p>This event would not be possible without the support of our generous sponsors. If your organization would like to support the Create-A-Thon as well, please <a href="#contact" class="page-scroll">let us know!</a></p>
 					<div class="logo-box">
-						<?php echo types_render_field("sponsors", array("output"=>"html")); ?>
+						<?php echo types_render_field("sponsor-content", array("output"=>"html")); ?>
 						
 					</div><!--.logo-box-->
 				</div>
