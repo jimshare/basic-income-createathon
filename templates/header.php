37c8d8b61b785005e7b7e22367aa5e2e510deefc
Justine Lam <justine.lam@gmail.com>
2016-01-19 12:37:25 -0800

    Change navigation for guide & content template pages
    - Changed the background of body for both pages to #000 and color #fff

--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,8 @@ if ( is_front_page() ) :
     get_template_part('templates/header', 'home');
 elseif ( is_page_template( 'template-content.php' ) ) :
     get_template_part('templates/header', 'about');
+elseif ( is_page_template( 'template-guide.php' ) ) :
+    get_template_part('templates/header', 'about');
 else:
     get_template_part('templates/header', 'createathon');
 endif;
