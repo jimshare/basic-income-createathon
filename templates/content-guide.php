25803282d2d7bc11ff1ed07d031c3c5d0707e9dc
Justine Lam <justine.lam@gmail.com>
2016-01-19 13:06:09 -0800

    Change the background for content template hero to white
    - Adding feature image as background for guide/organize template

--- templates/content-guide.php
+++ templates/content-guide.php
@@ -1,5 +1,7 @@
+<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+
     <!-- Intro Section -->
-    <section id="about" class="container content-section text-center">
+    <section id="about" class="container content-section text-center" style="background: url('<?php echo $image[0]; ?>') no-repeat center center scroll; background-size: cover; margin: 0;">
         <div class="row">
             <h1><?php the_title(); ?></h1>
             <div class="col-lg-8 col-lg-offset-2">
