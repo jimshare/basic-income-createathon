c09c68407b4a3279e866b88eab6b8e4cdd14be61
Jim Pugh <jim@shareprogress.org>
2017-11-06 20:47:05 -0800

    Updated Typeform link

--- templates/content-home.php
+++ templates/content-home.php
@@ -21,7 +21,7 @@
         <div class="container">
             <div class="col-lg-8 col-lg-offset-2">
                 <h2 class="large-title">Join the Network</h2>
-				<div class="typeform-widget" data-url="https://jimpugh.typeform.com/to/PkjeAc" data-text="Basic Income Survey" style="width:100%;height:500px;"></div>
+				<div class="typeform-widget" data-url="https://universalincome.typeform.com/to/PkjeAc" data-text="Basic Income Survey" style="width:100%;height:500px;"></div>
 				<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
 				<div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"><a href="https://www.typeform.com/examples/surveys/?utm_campaign=PkjeAc&amp;utm_source=typeform.com-3329337-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-onlinesurvey&amp;utm_term=EN" style="color: #999" target="_blank">Online survey</a> powered by Typeform</div>
             </div>
